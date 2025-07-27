2ef71a0985c7a1a793a0a06e182ea775e9baf90a
Githusigan2002 <jeyarupangithusigan@gmail.com>
2025-07-27 21:04:14 +0530

    fixed validations

--- models/movie.model.js
+++ models/movie.model.js
@@ -1,15 +1,15 @@
 import { model, Schema } from "mongoose";
 //Write schema
 const schema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  desc: {
-    type: String,
-    required: true,
-  },
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
 });
 
 //create model
